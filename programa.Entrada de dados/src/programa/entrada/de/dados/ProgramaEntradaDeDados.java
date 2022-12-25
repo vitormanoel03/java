2183f71c5656305086158a36fc857cf6a91e6155
Vitor Manoel <114440719+vitormanoel03@users.noreply.github.com>
2022-12-25 19:53:34 -0300

    Add files via upload

---
+++ programa.Entrada de dados/src/programa/entrada/de/dados/ProgramaEntradaDeDados.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa.entrada.de.dados;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc farias
+ */
+public class ProgramaEntradaDeDados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /* Scanner input = new Scanner(System.in);
+        System.out.println("Qual o seu nome ?");
+        String nome = input.nextLine();
+        System.out.println("Bem vindo ,"+ nome);
+        
+        System.out.println("Digite sua nota");
+        int nota = input.nextInt();
+        **
+        System.out.println(nome+", sua nota é "+nota);
+*/
+       
+    }
+    
+}
